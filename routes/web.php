8de3342c25a36925c7e3a59c7f1e19701298216a
GuoTingRui <azsxdc890902@gmail.com>
2021-11-20 18:54:41 +0800

    練習7:以Route串接Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -54,3 +55,5 @@ Route::group(['prefix' => 'admin'], function() {
         return 'admin dashboard';
     });
 });
+
+Route::get('home', [HomeController::class, 'index'])->name('home.index');

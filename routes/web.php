52a00ada8dcc6f2f3268d469853dba3c426256c3
GuoTingRui <azsxdc890902@gmail.com>
2021-11-20 17:35:42 +0800

    練習1-3:設定Route跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'welcome';
 });
+
+Route::get('r1', function () {
+    return redirect('r2');
+});
+
+Route::get('r2', function () {
+    return view('welcome');
+});
